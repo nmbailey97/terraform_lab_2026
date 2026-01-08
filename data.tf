52c05b831575be5f81e57e3cf84d3e0e9c8916d6
pmintconsulting <pmintcollectconsulting@gmail.com>
2026-01-08 17:21:09 +0000

    ec2 with something added

---
+++ data.tf
@@ -0,0 +1,46 @@
+data "aws_vpc" "day3example" {
+  id = "vpc-07eede624774fec02"
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name = "nathalie_allow_boa_rules"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id = data.aws_vpc.day3example.id
+
+  tags = {
+    Name = "nathalie_allow_boa_rules"
+  }
+}
+
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+ 
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+ 
+# ingress 22 and 80 port allow
+ 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+ 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
