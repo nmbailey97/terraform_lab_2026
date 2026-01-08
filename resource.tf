303d48fa0a554ee009e25489c85c50cbb2abe5a5
pmintconsulting <pmintcollectconsulting@gmail.com>
2026-01-08 20:48:01 +0000

    another edit for loops?

--- resource.tf
+++ resource.tf
@@ -1,10 +1,13 @@
 resource "aws_instance" "ec2" {
+  for_each = var.ec2_instances
   ami = var.nathalie-ami
-  instance_type = var.vm-size
-  count = var.novm
+  instance_type = each.value
+  #count = var.novm
+
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   
   tags = { 
-    Name = "${var.vm-name}-${count.index}"
+    #Name = "${var.vm-name}-${count.index}"
+    Name = "nathalie-vm-${each.key}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,12 @@ variable "novm" {
   type = number
   description = "this is for number of vms being created"
 }
+
+
+variable "ec2_instances" {
+  type = map(string)
+  default = {
+    "web" = "t3.micro"
+    "db" = "t2.small"
+  }
+}
